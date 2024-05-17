3f9ac8de5f8737d9fa60d487fb76b1689c8d5b4b
higor-brito <higorbrito8200@gmail.com>
2024-05-17 10:34:24 -0300

    feat: criando estrutura do projeto

---
+++ src/main/java/org/serratec/backend/individaualAPI/IndividaualApiApplication.java
@@ -0,0 +1,13 @@
+package org.serratec.backend.individaualAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividaualApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndividaualApiApplication.class, args);
+	}
+
+}
